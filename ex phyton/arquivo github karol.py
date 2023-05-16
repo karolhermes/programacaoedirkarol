d6065d2e8ea0956813a1a5a66add18fb94dbfff6
karolhermes <126607596+karolhermes@users.noreply.github.com>
2023-05-16 15:19:55 -0300

    Add files via upload

--- ex phyton/arquivo github karol.py
+++ ex phyton/arquivo github karol.py
@@ -17,3 +17,25 @@ def hipotenusa ( cateto1, cateto2 ):
     return soma( quadrado(cateto1 ) , quadrado( cateto2 ) ) ** ( 1 / 2 )
 
 print (soma(2,3))
+
+# estruturas de decisão 
+
+def simples(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+
+def medio (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    else:
+        return 'tente outra vez'
+    
+    def completo (cor):
+        if cor == 'azul':
+            return 'escolheu certo' 
+        elif cor == 'marrom' :
+            return 'não tem salvação'
+        else:
+            return 'tente outra vez'
+        
+    
